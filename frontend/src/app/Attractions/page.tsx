f2a0c6b7862b2dff009f91f3765d92307d8a8710
root <biuro.norsk@gmail.com>
2024-09-25 06:36:59 +0200

    Twoje zmiany w Attractions/page.tsx

--- frontend/src/app/Attractions/page.tsx
+++ frontend/src/app/Attractions/page.tsx
@@ -3,12 +3,11 @@
 import { CiSearch } from 'react-icons/ci';
 import AttractionCard from '../components/AttractionCard';
 import SmallBlogCard from '../components/SmallBlogCard';
-import Script from 'next/script';
+import CheckfrontWidget from '@/components/checkfront';
 
 export default function AttractionsPage() {
   return (
     <>
-      {/* Sekcja tła z formularzem wyszukiwania */}
       <div className='h-[500px] relative flex items-end justify-center bg-[url(/img/AboutUs.webp)] bg-bottom bg-cover'>
         <div className='absolute top-0 left-0 w-full h-full bg-gradient-to-t from-black/50 to-transparent -z-0'></div>
         <div className='mb-24 z-10'>
@@ -32,53 +31,38 @@ export default function AttractionsPage() {
         </div>
       </div>
 
-      {/* Wyświetlanie kart atrakcji */}
       <div className='max-w-screen-xl mx-auto text-black'>
         <div className='grid grid-cols-1 md:grid-cols-3 my-8 gap-6'>
           <SmallBlogCard />
-          {/* Trollsjön */}
+          
           <AttractionCard
-            title='Trollsjön'
+            title="Trollsjön"
             rating={4.8}
-            location='Laponia'
-            duration='3 godziny'
+            location="Laponia"
+            duration="3 godziny"
             price={250}
-            widgetId='CHECKFRONT_WIDGET_01'
-            itemId='2' // ID dla Trollsjön
-            categoryId='2'
           />
+          <CheckfrontWidget widgetId="CHECKFRONT_WIDGET_01" itemId="6" categoryId="2" />
 
-          {/* Fjords */}
           <AttractionCard
-            title='Fjords'
+            title="Fjords"
             rating={4.7}
-            location='Norwegia'
-            duration='5 godzin'
+            location="Norwegia"
+            duration="5 godzin"
             price={400}
-            widgetId='CHECKFRONT_WIDGET_02'
-            itemId='3' // ID dla Fjords
-            categoryId='3'
           />
+          <CheckfrontWidget widgetId="CHECKFRONT_WIDGET_02" itemId="31,32" categoryId="2" />
 
-          {/* Hikes in Abisko */}
           <AttractionCard
-            title='Hikes in Abisko'
+            title="Aurora Snowmobile"
             rating={4.9}
-            location='Abisko'
-            duration='8 godzin'
+            location="Abisko"
+            duration="8 godzin"
             price={350}
-            widgetId='CHECKFRONT_WIDGET_03'
-            itemId='4' // ID dla Hikes in Abisko
-            categoryId='2'
           />
+          <CheckfrontWidget widgetId="CHECKFRONT_WIDGET_03" itemId="38,39" categoryId="3" />
         </div>
       </div>
-
-      {/* Skrypt Checkfront */}
-      <Script
-        src='//explorearctic.checkfront.com/lib/interface--0.js'
-        strategy='beforeInteractive'
-      />
     </>
   );
 }
