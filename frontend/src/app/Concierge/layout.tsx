807f247ac425c8a170c4b42e67aa29933920262e
Budzisz77 <michal.budziszewski86@gmail.comm>
2025-07-23 14:34:45 +0200

    concierge

--- frontend/src/app/Concierge/layout.tsx
+++ frontend/src/app/Concierge/layout.tsx
@@ -1,34 +1,16 @@
 import type { Metadata } from 'next';
 import Footer from '../components/Footer';
 
-<<<<<<< HEAD
-export const metadata: Metadata = {
-  title: 'Abisko | Guides',
-  description: 'Take Your Journey and Discover the Magic of Lapland',
-};
-
-export default function PageLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-=======
-
 export const metadata: Metadata = {
   title: 'Abisko | Concierge',
   description: 'Take Your Journey and Discover the Magic of Lapland',
 };
 
 export default function PageLayout({ children }: { children: React.ReactNode }) {
->>>>>>> e91bdd4b4c90367c7a21d3313695ce1751186240
   return (
     <>
       {children}
       <Footer page='' />
     </>
   );
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> e91bdd4b4c90367c7a21d3313695ce1751186240
