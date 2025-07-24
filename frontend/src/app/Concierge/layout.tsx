58900a58bb299bdece6d3145179e64a0f93f52a8
Jarosław Karpiński <biuro.norsk@gmail.com>
2025-07-24 18:14:49 +0200

    asd

--- frontend/src/app/Concierge/layout.tsx
+++ frontend/src/app/Concierge/layout.tsx
@@ -1,10 +1,6 @@
 import type { Metadata } from 'next';
 import Footer from '../components/Footer';
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 2988218 (added raport.md)
 export const metadata: Metadata = {
   title: 'Abisko | Concierge',
   description: 'Take Your Journey and Discover the Magic of Lapland',
