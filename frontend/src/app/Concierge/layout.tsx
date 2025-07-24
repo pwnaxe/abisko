d569d536b7bc616756504e19a48420074075df73
root <biuro.norsk@gmail.com>
2025-07-24 18:08:39 +0200

    added raport.md

--- frontend/src/app/Concierge/layout.tsx
+++ frontend/src/app/Concierge/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from 'next';
 import Footer from '../components/Footer';
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> 2988218 (added raport.md)
 export const metadata: Metadata = {
   title: 'Abisko | Concierge',
   description: 'Take Your Journey and Discover the Magic of Lapland',
